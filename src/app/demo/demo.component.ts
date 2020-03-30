7f6f79e90710b856e81516d19a79d46feea70c7a
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2020-03-31 01:48:41 +0530

    sharing data between child to parent issue fixed

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 // import { EventEmitter } from 'protractor';
 
@@ -8,26 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
-  @Output() emitevent=new EventEmitter<{typeName:string,servertypeName:string}>();
+  @Output() emitevent = new EventEmitter<{ typeName: string, servertypeName: string }>();
   //  message:string="hello";
   // @Output() messageEvent=new EventEmitter<string>();
   // name: string="";
   // type:string="";
   // servertype:string="";
-    newname="";
-    newname1="";
+  newname = "";
+  newname1 = "";
 
-  emits(){
-this.emitevent.emit({typeName:this.newname,servertypeName:this.newname1})
+  emits() {
+    console.log(this.newname);
+    console.log(this.newname1);
+    this.emitevent.emit({ typeName: this.newname, servertypeName: this.newname1 })
   }
 
-  emits1(){
-    this.emitevent.emit({typeName:this.newname,servertypeName:this.newname1})
-      }
-// sendmeassage(){
-//   this.messageEvent.emit(this.message);
-// }
-  constructor(private route:Router) { }
+  emits1() {
+    this.emitevent.emit({ typeName: this.newname, servertypeName: this.newname1 })
+  }
+  // sendmeassage(){
+  //   this.messageEvent.emit(this.message);
+  // }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
